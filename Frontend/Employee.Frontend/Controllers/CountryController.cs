971fb9a5fc30be0e9e3e4ad4049fdea5d41878ef
Your Namemdsazzad30 <you@example.commdsazzad304050@gmail.com>
2023-09-30 12:04:56 +0600

    Frontend

---
+++ Frontend/Employee.Frontend/Controllers/CountryController.cs
@@ -0,0 +1,73 @@
+﻿using Employee.Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Frontend.Controllers;
+
+public class CountryController : Controller
+{
+    private readonly HttpClient _httpClient;
+    public CountryController(IHttpClientFactory httpClientFactory) => _httpClient = httpClientFactory.CreateClient("EmployeeApiBase");
+
+    public async Task<IActionResult> Index() => View(await GetAllCountry());
+
+    public async Task<List<Country>> GetAllCountry()
+    {
+        var response = await _httpClient.GetFromJsonAsync<List<Country>>("Country");
+        return response is not null ? response : new List<Country>();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddOrEdit(int Id)
+    {
+        if (Id == 0)
+        {
+            //Creat From
+            return View(new Country());
+        }
+        else
+        {
+            // Find By Id
+            var data = await _httpClient.GetAsync($"Country/{Id}");
+            if (data.IsSuccessStatusCode)
+            {
+                var result = await data.Content.ReadFromJsonAsync<Country>();
+                return View(result);
+            }
+        }
+        return View(new Country());
+    }
+
+    [HttpPost]
+    [AutoValidateAntiforgeryToken]
+    public async Task<IActionResult> AddOrEdit(int Id, Country country)
+    {
+        if (ModelState.IsValid)
+        {
+            if (Id == 0)
+            {
+                var result = await _httpClient.PostAsJsonAsync("Country", country);
+                if (result.IsSuccessStatusCode) return RedirectToAction("Index");
+            }
+            else
+            {
+                var result = await _httpClient.PutAsJsonAsync($"Country/{Id}", country);
+                if (result.IsSuccessStatusCode) return RedirectToAction("Index");
+            }
+        }
+        return View(new Country());
+    }
+    public async Task<IActionResult> Delete(int Id)
+    {
+        var data = await _httpClient.DeleteAsync($"Country/{Id}");
+        if (data.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(new Country());
+        }
+    }
+}
+
+
---
+++ Frontend/Employee.Frontend/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Employee.Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Employee.Frontend.Controllers;
+
+public class EmployeeController : Controller
+{
+    private readonly HttpClient _httpClient;
+    public EmployeeController(IHttpClientFactory httpClientFactory) => _httpClient = httpClientFactory.CreateClient("EmployeeApiBase");
+
+    public async Task<IActionResult> Index()
+    {
+        var data = await GetAllEmployee();
+        return View(data);
+    }
+    public async Task<IEnumerable<Employeess>> GetAllEmployee()
+    {
+        var response = await _httpClient.GetAsync("Employee");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var emp = JsonConvert.DeserializeObject<IEnumerable<Employeess>>(content);
+            return emp;
+        }
+        return new List<Employeess>();
+    }
+}
